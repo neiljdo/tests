f23f947b290e637796643602472fbc5b71ab13c9
Neil John D. Ortega <neiljohn.ortega@gmail.com>
2013-07-21 22:57:28 +0800

    PostgreSQL adventure (basic stuff)

---
+++ postgresql/drop_tables-bpsimple.sql
@@ -0,0 +1,12 @@
+DROP TABLE barcode;
+DROP TABLE orderline;
+DROP TABLE stock;
+DROP TABLE orderinfo;
+DROP TABLE item;
+DROP TABLE customer;
+
+/* PostgreSQL automatically drops the sequences now...
+DROP SEQUENCE customer_customer_id_seq;
+DROP SEQUENCE item_item_id_seq;
+DROP SEQUENCE orderinfo_orderinfo_id_seq;
+*/
---
+++ postgresql/reset_project-bpsimple.sql
@@ -0,0 +1,3 @@
+\i '/home/neil/Documents/tests/postgresql/drop_tables-bpsimple.sql'
+\i '/home/neil/Documents/tests/postgresql/create_tables-bpsimple.sql'
+\i '/home/neil/Documents/tests/postgresql/pop_tables-bpsimple.sql'
